fb15c22c4aff9188599c0ade6d90b91ad75c1cb2
juan <chavezvasquezjuan@gmail.com>
2019-08-13 15:11:42 -0500

    getAffordable booking

--- src/main/java/com/example/springboot/BookingController.java
+++ src/main/java/com/example/springboot/BookingController.java
@@ -1,11 +1,13 @@
 package com.example.springboot;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/bookings")
@@ -19,6 +21,12 @@ public class BookingController {
         bookings.add(new HotelBooking("Marriot", 200.50, 3));
         bookings.add(new HotelBooking("Ibis", 200.50, 4));
         bookings.add(new HotelBooking("Novotel", 200.50, 1));
+    }
+
+    @RequestMapping(value = "/affordable/{price}", method = RequestMethod.GET)
+    public List<HotelBooking> getAffordable(@PathVariable double price) {
+        return bookings.stream().filter(x -> x.getPricePerNight() <= price)
+                .collect(Collectors.toList());
     }
 
     @RequestMapping(value = "/all", method = RequestMethod.GET)
