b8767461ba46aa63f723acd38cc51f6a910ceaaa
juan <chavezvasquezjuan@gmail.com>
2019-08-14 20:21:40 -0500

    templates

--- src/main/java/com/example/springboot/ViewController.java
+++ src/main/java/com/example/springboot/ViewController.java
@@ -1,13 +1,20 @@
 package com.example.springboot;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Date;
 
 @Controller
 public class ViewController {
 
     @RequestMapping("/")
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("datetime", new Date());
+        model.addAttribute("username", "Pomain Coder");
+        model.addAttribute("mode", "development");
+
         return "index";
     }
 
